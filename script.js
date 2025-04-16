fcf2703c530082621fb83c8a6e69e03fa4e4fa17
Orestis Tsatsos <orestis@Orestiss-MacBook-Air.local>
2025-04-16 22:03:08 +0300

    updated UI and added an alert for empty input)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const validateBtn = document.getElementById("validate-btn");
 const inputString = document.getElementById("string-input");
 const resultsMsg = document.getElementById("results-message");
-const regex = /[^a-zA-Z]/g;
+const regex = /[^a-zA-Z0-9]/g;
 
 const cleanInput = (input) => {
     const word = input.replace(regex,"");
@@ -17,13 +17,18 @@ const compareStrings = (input,word) => {
 }
 
 const validatePalindrome = () => {
-    const cleanWord=cleanInput(inputString.value);
-    const reversedWord =reverseInput(cleanWord);
-    if(compareStrings(cleanWord,reversedWord)){
-        resultsMsg.innerHTML = `${inputString.value} is a palindrome.`;
+    if(inputString.value===""){
+        alert("Please insert a valid input.")
     } else {
-        resultsMsg.innerText = `${inputString.value} is not a palindrome.`;
+        const cleanWord=cleanInput(inputString.value);
+        const reversedWord =reverseInput(cleanWord);
+        if(compareStrings(cleanWord,reversedWord)){
+            resultsMsg.innerHTML = `${inputString.value} is a palindrome.`;
+    }   else {
+            resultsMsg.innerText = `${inputString.value} is not a palindrome.`;
     }
+    }
+    
 }
 
 validateBtn.addEventListener("click",validatePalindrome);
